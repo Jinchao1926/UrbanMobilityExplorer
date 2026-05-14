975acefff084a436e9e4f822b8a05da70aa90625
Jinchao Lin <jinchao.1926@gmail.com>
2026-05-14 20:25:14 +0800

    Initialize SwiftUI project

---
+++ UrbanMobilityExplorer/UrbanMobilityExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanMobilityExplorerApp.swift
+//  UrbanMobilityExplorer
+//
+//  Created by Jinchao Lin on 2026/5/14.
+//
+
+import SwiftUI
+
+@main
+struct UrbanMobilityExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
